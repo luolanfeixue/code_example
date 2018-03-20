5f01b593b68efb4f80819f39bdd493ce76d3278d
hlhuang <hlhuang@yangqianguan.com>
2018-03-20 13:27:42 +0800

    python 参数配置

---
+++ pytnon_args_click.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-7 -*-
+#
+# Author : hhl
+#
+# Time : Sat 17 Mar 2018 19:14:25
+#
+#
+import click
+ 
+@click.command()
+@click.option('--count', default=1, help='Number of greetings.')
+@click.option('--name', prompt='Your name', help='The person to greet.')
+def hello(count, name):
+    """Simple program that greets NAME for a total of COUNT times."""
+    print(count,name)
+ 
+if __name__ == '__main__':
+    hello()
