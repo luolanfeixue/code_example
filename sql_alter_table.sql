bfce1ab8c72a2cf46fd60c07a42d1692218bc3c1
huanghailong9 <huanghailong@jd.com>
2018-03-22 14:58:04 +0800

    python md5

--- sql_alter_table.sql
+++ sql_alter_table.sql
@@ -10,7 +10,9 @@ CREATE TABLE test_change (a int, b int, c int);
 --will change column a's name to a1, a's data type to string, and put it after column b. 
 --The new table's structure is: b int, a1 string, c inti
 ALTER TABLE test_change CHANGE a a1 STRING AFTER b; 
-
 --will change column b's name to b1, and put it as the first column. 
 --The new table's structure is: b1 int, a string, c int
 ALTER TABLE test_change CHANGE b b1 INT FIRST;
+
+--更改表的存储格式
+alter table btcf.recommend_lda_order_result SET FILEFORMAT textfile;
